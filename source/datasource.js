611681976a383edf5dedf7d23f3fea70fbad9c02
Garbrand van der Molen <garbrand@gmail.com>
2016-04-01 10:04:44 +0200

    Initial commit

---
+++ source/datasource.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function DataSourceFactory() {
+	// DataSourceFactory extends Array, and wraps 'push' so it can be used for pub/sub.
+	let collection = new Array( ...arguments );
+
+	// Mixing in custom methods to Array:
+	Object.defineProperties( collection, {
+		'push': {
+			enumerable: false,
+			value: (data) => {
+				// Add data to the datasource
+				Array.prototype.push.call( collection, data );
+
+				// Call each subscriber with the new collection & data
+				collection.__subscribers.forEach( (fn) => fn( collection, data ) );
+			}
+		},
+		'subscribe': {
+			enumerable: false,
+			value: ( fn ) => {
+				if( typeof fn !== 'function' ) throw new TypeError('You can only subscribe functions');
+				collection.__subscribers.push( fn );
+			}
+		},
+		'__subscribers': {
+			enumerable: false,
+			value: []
+		}
+	});
+
+	return collection;
+}
---
+++ test/datasource.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let DS     = require( '../source/datasource.js' );
+let expect = require('chai').expect;
+
+describe( 'Datasource:', () => {
+	describe( '(extending Array)', () => {
+		let ds = DS();
+
+		it('should be of type Array', () => {
+			expect( ds ).to.be.instanceof( Array );
+		});
+
+		it('should not expose any custom methods or properties', () => {
+			expect( Object.keys( ds ) ).to.eql( Object.keys( new Array() ) );
+		});
+	});
+
+	describe( '(pub/sub)', () => {
+		it('should register functions as subscribers', ( ) => {
+			let ds = DS();
+
+			expect( ds.__subscribers.length ).to.equal( 0 );
+			ds.subscribe( () => {} );
+			expect( ds.__subscribers.length ).to.equal( 1 );
+		});
+
+		it('should not register anything else then functions as subscribers', () => {
+			let ds = DS();
+
+			expect( ds.subscribe ).to.throw( TypeError );
+		});
+
+		it('should call the subscribers when the Datasource changes', ( done ) => {
+			let ds = DS();
+
+			ds.subscribe( ( array, item ) => {
+				expect( array ).to.eql ( [ 1 ]);
+				expect( item ).to.equal( 1 );
+				done();
+			} );
+
+			ds.push( 1 );
+		});
+	});
+
+	describe( 'API', () => {
+		let state = [ 1, 2, 3 ];
+		let change = 4;
+
+		let ds = DS( 1,2,3 );
+
+		it('should return the new state of the Datasource as the first argument and the change as the second', ( done ) => {
+			function handler( state, change ) {
+				return function( array, item ) {
+					expect( array ).to.not.eql( state );
+					expect( array ).to.eql( [ 1 , 2 , 3 , 4 ] );
+					expect( item ).to.eql( change );
+					done();
+				}
+			}
+
+			expect( ds ).to.eql( state );
+			ds.subscribe( handler( state, change ) );
+			ds.push( 4 );
+		});
+	});
+} );
